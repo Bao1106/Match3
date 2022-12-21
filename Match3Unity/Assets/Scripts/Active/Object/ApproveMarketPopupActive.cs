74575a6267811fec3777412c761f7020cca2358f
Bao1106 <quocbaovo06@gmail.com>
2022-12-21 09:09:16 +0700

    Update base script

---
+++ Match3Unity/Assets/Scripts/Active/Object/ApproveMarketPopupActive.cs
@@ -0,0 +1,14 @@
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ApproveMarketPopupActive : RxActive
+//{
+//    WalletService walletService = WalletService.Instance;
+
+//    protected override void ObservationTargetDesignation()
+//    {
+//        objectObservation = walletService.approveMarketPopupActive;
+//    }
+//}
---
+++ Match3Unity/Assets/Scripts/Button/BtnOpenWithdrawNotice.cs
@@ -0,0 +1,13 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class BtnOpenWithdrawNotice : RxButton
+//{
+//    [SerializeField] private RectTransform WithdrawNoticePopup;
+//    public override void OnClickAsync()
+//    {
+//        WithdrawNoticePopup.gameObject.SetActive(true);
+//    }
+//}
+
